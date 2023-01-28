3e7a98a09ab06f67e4956991b346fb61ebe4468a
Robbin Janssen <r.janssen@exonet.nl>
2023-01-28 22:06:03 +0100

    Add duration support for comfort mode

--- tests/test_request_update_group.py
+++ tests/test_request_update_group.py
@@ -52,9 +52,9 @@ async def test_update_regulation_mode_comfort() -> None:
     request = UpdateGroupRequest(resource=thermostat, api_key="v3ry-s3cr3t")
     result = request.update_regulation_mode(REGULATION_COMFORT)
 
-    # Assert comfort end time is the current date + 4 hours.
+    # Assert comfort end time is the current date + 240 minutes.
     assert result["SetGroup"]["ComfortEndTime"] == (
-        datetime.now() + timedelta(hours=4)
+        datetime.now() + timedelta(minutes=240)
     ).strftime(dateformat)
 
     # Assert rest is the same.
@@ -64,6 +64,44 @@ async def test_update_regulation_mode_comfort() -> None:
 
     # Assert the temperature is the same.
     assert result["SetGroup"]["ComfortSetpoint"] == 2000
+
+
+@pytest.mark.asyncio
+@freeze_time("2023-01-01 11:30:35")
+async def test_update_regulation_mode_comfort_with_temp_and_duration() -> None:
+    """
+    Test that the regulation mode can be set to comfort.
+
+    Make sure the end times are set correctly.
+    """
+    data = load_fixtures("single_thermostat.json")
+    thermostat = Thermostat.from_json(json.loads(data))
+    dateformat = "%Y-%m-%dT%H:%M:%S"
+
+    # Check the current times.
+    assert thermostat.comfort_end_time.strftime(dateformat) == "2022-12-28T15:58:34"
+    assert thermostat.boost_end_time.strftime(dateformat) == "2023-01-02T14:55:01"
+    assert thermostat.vacation_begin_time.strftime(dateformat) == "2022-12-21T00:00:00"
+    assert thermostat.vacation_end_time.strftime(dateformat) == "2022-12-22T00:00:00"
+
+    # Check the current temperature for comfort.
+    assert thermostat.temperatures[REGULATION_COMFORT] == 2000
+
+    request = UpdateGroupRequest(resource=thermostat, api_key="v3ry-s3cr3t")
+    result = request.update_regulation_mode(REGULATION_COMFORT, 2350, 360)
+
+    # Assert comfort end time is the current date + 360 minutes.
+    assert result["SetGroup"]["ComfortEndTime"] == (
+        datetime.now() + timedelta(minutes=360)
+    ).strftime(dateformat)
+
+    # Assert rest is the same.
+    assert result["SetGroup"]["BoostEndTime"] == "2023-01-02T14:55:01"
+    assert result["SetGroup"]["VacationBeginDay"] == "2022-12-21T00:00:00"
+    assert result["SetGroup"]["VacationEndDay"] == "2022-12-22T00:00:00"
+
+    # Assert the temperature is the same.
+    assert result["SetGroup"]["ComfortSetpoint"] == 2350
 
 
 @pytest.mark.asyncio
